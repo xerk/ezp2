67083b3eaa2a879f30c00976c8a6310ded72cffb
Ahmed Mamdouh <gm.xerk@gmail.com>
2019-04-10 16:43:48 +0200

    a

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,8 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/user', function (Request $request) {
-        return $request->user()->with('orders');
+         $user = $request->user();
+         return App\User::with('orders')->find($user);
     });
     Route::post('/logout', 'AuthController@logout');
     Route::post('/order', 'ApiOrderController@order');
